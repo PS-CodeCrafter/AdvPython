bfe0f97ed78d9c519e624b2caa63ea9bfd93df9f
Prisha <prishas2007@gmail.com>
2025-09-14 14:43:09 -0700

    edited more_iterators.py

--- Start/2. Iterators/more_iterators.py
+++ Start/2. Iterators/more_iterators.py
@@ -1,21 +1,30 @@
 # Example file for Advanced Python by Joe Marini
-
+import itertools
 
 # define a list of days in English and French
 days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-daysFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
+daysFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven"]
 
+# for d in range(len(days)):
+#   print(d+1,days[d])
+  
 # the enumerate function
-
+# for i, d in enumerate(days, start=1):
+#   print(i,d)
 
 # use zip to combine sequences
-
+# for d in zip(days, daysFr):
+#   print(d)
 
 # use enumerate and zip together
-
+# for i, d in enumerate(zip(days,daysFr), start=1):
+#   print(i,d[0],"=",d[1], "in French")
 
 # use zip_longest
 seq1 = ["A","B","C","D","E","F"]
 seq2 = [1, 2, 3, 4]
 seq3 = "xyz"
-    
+result = itertools.zip_longest(seq1,seq2,seq3, fillvalue="-")
+print("Result: ")
+for item in result:
+  print(item)    
